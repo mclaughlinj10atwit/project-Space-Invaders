6c363cd52a351cff29535b65f22dbc3f3bf11174
Jared McLaughlin <Jared McLaughlin@DESKTOP-TC4167C.wit.private>
2023-04-10 08:16:54 -0700

    all

---
+++ src/application/AlienInstructions.java
@@ -0,0 +1,97 @@
+package application;
+
+import java.util.ArrayList;
+
+import javafx.animation.Transition;
+import javafx.animation.TranslateTransition;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+public class AlienInstructions extends Enemymovement {
+	private int x;
+	private int y;
+	private int w;
+	private int h;
+	private Rectangle Alien;
+	boolean moveRight;
+	boolean moveLeft;
+	boolean isvisible;
+	
+		 // TODO Auto-generated constructor stub
+//		public	AlienInstructions(int x, int y, int w, int h) {
+//			super(x,y,w,h);
+//			this.x = x;
+//			this.y = y;
+//		}
+	Rectangle [] array = new Rectangle[26];
+	static ArrayList<Rectangle> enemy = new ArrayList<Rectangle>();
+
+		public  AlienInstructions(int x,int y,int w,int h) {
+
+			int j = 0;
+			for (int i = 1; i < 6; i++) {
+				 Alien = new Rectangle(i * 100, 50 + y, 50, 30);
+				 enemy.add(Alien);
+
+			//	movement(i * 50);
+				if (i == 5) {
+					y = y + 60;
+					if (j != 4) {
+						i = 0;
+					}
+					j++;
+				}
+				
+
+			}
+//			for (int i = 1; i < enemy.size() + 1; i++) {
+//				 TranslateTransition translateTransition = new TranslateTransition();
+//				 translateTransition.setNode(enemy.get(i));
+//				 translateTransition.setDuration(Duration.seconds(4));
+//				
+//				translateTransition.setAutoReverse(true);
+//					translateTransition.setToX( x + 1);
+//
+//				
+//				translateTransition.setFromX(400);
+//
+//				
+//				translateTransition.setCycleCount(Transition.INDEFINITE);
+//				translateTransition.play();
+//			}
+		}
+	
+		public int getTranslateX() {
+			return x ;
+			
+		}
+		public int getTranslateY() {
+			return y ;
+			
+		}
+		public int  enemyMovement() {
+			return y+ 50;
+		}
+		public static ArrayList<Rectangle> getAlien() {
+			return enemy;
+		}
+}
+
+//	public static class AlienInstructions extends Rectangle {
+//
+//	public	AlienInstructions(int x, int y, int w, int h) {
+//			super(x, y, w, h);
+//			setTranslateX(x);
+//			setTranslateY(y);
+//		}
+//
+//		void enemyMovement() {
+//			setTranslateY(getTranslateY() + 5);
+//		}
+//		void getTranslateX() {
+//			return setTranslate(x);
+//		}
+//	}
+//}
+
+
